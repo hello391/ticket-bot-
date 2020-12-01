82045708f160e4181559ae2ab377a8c5bc96e08c
hello391 <73004310+hello391@users.noreply.github.com>
2020-12-01 16:59:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 require('dotenv').config();
 const keepAlive = require('./server');
-const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ disableMentions: 'everyone', partials: ['MESSAGE', 'CHANNEL', 'REACTION'], ws: { intents: Intents.ALL } });
+const {
+    Client,
+    Collection,
+    Intents,
+    MessageEmbed
+} = require('discord.js');
+const client = new Client({
+    disableMentions: 'everyone',
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    ws: {
+        intents: Intents.ALL
+    }
+});
 
 client.commands = new Collection();
 client.aliases = new Collection();
 
 ['command', 'event'].forEach(handler => {
-	require(`./handlers/${handler}`)(client);
+    require(`./handlers/${handler}`)(client);
 });
 
+client.on('guildCreate', guild => {
+    const e = new MessageEmbed()
+        .setTitle('Ticket Bot')
+        .setFooter(`${client.user.username}'s Info`, `${client.user.avatarURL()}`)
+        .setDescription('My prefix is t! to get started type t!help any role with admin will have acces to the tickets')
+        .setColor('BLUE')
+
+
+
+    guild.owner.send(e)
+})
 keepAlive();
 client.login(process.env.BOT_TOKEN);
